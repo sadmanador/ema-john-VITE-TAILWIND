0840e120b4447d6878a19a17a0d2672b4e5624d9
sadmanador <sadman.ador@gmail.com>
2023-05-01 01:47:30 +0600

    loader changed

--- src/routes/PrivateRoute/PrivateRoute.jsx
+++ src/routes/PrivateRoute/PrivateRoute.jsx
@@ -6,7 +6,12 @@ const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <progress className="progress w-56"></progress>;
+    return (
+      <button type="button" class="bg-indigo-500 ..." disabled>
+        <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+        Loading...
+      </button>
+    );
   }
 
   if (user && user.uid) {
